5e22eeb15284d66c234c3bdb081fca5c37d097c5
祖士洋 <zushiyang@idataway.com>
2022-12-12 15:53:47 +0800

    📝 docs: modify note

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -20,7 +20,6 @@ export function createVitePlugins(isBuild: boolean, env: any) {
 
   vitePlugins.push(configSvgIconsPlugin());
 
-  //由于开启图片压缩后如果遇到比较大的图片会十分耗时，可以在此处选择性使用  pnpm run build:imgmin
   VITE_USE_IMAGEMIN === 'true' && vitePlugins.push(configImageminPlugin());
 
   return vitePlugins;
